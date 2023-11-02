2387aaeb3d2e81049db16329f2b4930bfad4abd4
bondtrinh1 <88116403+bondtrinh1@users.noreply.github.com>
2023-11-02 23:15:47 +1300

    Change classname for card parent container... again

--- src/components/Joke.tsx
+++ src/components/Joke.tsx
@@ -1,9 +1,8 @@
 import { Jokes } from "../types";
 
-function Joke({ id, punchline, setup, ...props }: Jokes) {
+function Joke(joke: Jokes) {
   return (
-    <div className={`joke-card ${id}`}>
-      {/* <span>Type: {type}</span> */}
+    <div className="joke">
       <div>
         {setup} <p>{punchline}</p>
       </div>
