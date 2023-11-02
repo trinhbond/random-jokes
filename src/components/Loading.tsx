6c01ff0a3f64e71f7640c82eee67b44a1c7d7952
bondtrinh1 <88116403+bondtrinh1@users.noreply.github.com>
2023-11-02 15:10:34 +1300

    Use classnames lib

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,11 +1,13 @@
-interface Sizes {
+import classNames from "classnames";
+
+type Sizes = {
   size: "small" | "medium" | "large";
-}
+};
 
 function Loading({ size }: Sizes) {
   return (
     <div className="spinner-parent">
-      <div className={`spinner ${size}`}>
+      <div className={classNames("spinner", size)}>
         <div></div>
         <div></div>
         <div></div>
