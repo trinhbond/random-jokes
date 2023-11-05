e76fe2095a1284a0bd1e68aeb21d264a1076ade1
bondtrinh1 <88116403+bondtrinh1@users.noreply.github.com>
2023-11-05 14:01:48 +1300

    Add QueryClientProvider to app

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,15 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./sass/index.scss";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(<App />);
+root.render(
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>
+);
