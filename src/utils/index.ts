2e3002533c9f8493989544de651dc77faed386f7
bondtrinh1 <88116403+bondtrinh1@users.noreply.github.com>
2023-11-05 13:55:58 +1300

    Add new helper to utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,12 @@
+import axios from "axios";
+
 export const capitalize = (word: string) =>
   word[0].toUpperCase() + word.substring(1);
+
+export const fetchJokes = async () => {
+  const { data } = await axios.get(
+    "https://official-joke-api.appspot.com/random_ten"
+  );
+
+  return data;
+};
