c81667176b0215697257a0f28a36624a54fe001e
bondtrinh1 <88116403+bondtrinh1@users.noreply.github.com>
2023-11-21 16:19:00 +1300

    Implicitly return loading

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -5,25 +5,23 @@ type Size = {
   size: "sm" | "md" | "lg";
 };
 
-function Loading({ size }: Size) {
-  return (
-    <Box
-      sx={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        flexDirection: "column",
-        height: "100vh",
-      }}
-    >
-      <CircularProgress
-        className={classNames("loading", size)}
-        sx={{ display: "inline-block", position: "relative" }}
-        size={size}
-        thickness={1.8}
-      />
-    </Box>
-  );
-}
+const Loading = ({ size }: Size) => (
+  <Box
+    sx={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      flexDirection: "column",
+      height: "100vh",
+    }}
+  >
+    <CircularProgress
+      className={classNames("loading", size)}
+      sx={{ display: "inline-block", position: "relative" }}
+      size={size}
+      thickness={1.8}
+    />
+  </Box>
+);
 
 export default Loading;
